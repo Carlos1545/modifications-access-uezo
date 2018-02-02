cf78f1a51225e0f1e23b9bfb0a7ebf592d4b2667
Carlos1545 <carlos.jr1545@gmail.com>
2018-02-02 21:57:37 -0200

    inserção de novo form de login

--- js/js-modifications.js
+++ js/js-modifications.js
@@ -1,11 +1,44 @@
 function somente_numero(campo){
-		var digits="0123456789"
-		var campo_temp
-		for (var i=0;i<campo.value.length;i++){
-		  campo_temp=campo.value.substring(i,i+1)   
-		  if (digits.indexOf(campo_temp)==-1){
-				campo.value = campo.value.substring(0,i);
-				break;
-		   }
-		}
+	var digits="0123456789"
+	var campo_temp
+	for (var i=0;i<campo.value.length;i++){
+	  campo_temp=campo.value.substring(i,i+1)   
+	  if (digits.indexOf(campo_temp)==-1){
+			campo.value = campo.value.substring(0,i);
+			break;
+	   }
 	}
+}
+(function() {
+				// trim polyfill : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim
+				if (!String.prototype.trim) {
+					(function() {
+						// Make sure we trim BOM and NBSP
+						var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+						String.prototype.trim = function() {
+							return this.replace(rtrim, '');
+						};
+					})();
+				}
+
+				[].slice.call( document.querySelectorAll( 'input.input__field' ) ).forEach( function( inputEl ) {
+					// in case the input is already filled..
+					if( inputEl.value.trim() !== '' ) {
+						classie.add( inputEl.parentNode, 'input--filled' );
+					}
+
+					// events:
+					inputEl.addEventListener( 'focus', onInputFocus );
+					inputEl.addEventListener( 'blur', onInputBlur );
+				} );
+
+				function onInputFocus( ev ) {
+					classie.add( ev.target.parentNode, 'input--filled' );
+				}
+
+				function onInputBlur( ev ) {
+					if( ev.target.value.trim() === '' ) {
+						classie.remove( ev.target.parentNode, 'input--filled' );
+					}
+				}
+			})();
